7c8e663bef2fb216fc00c6da921dca3be63b8fce
eshapatel2005 <esha.patel5605@gmail.com>
2026-09-10 14:55:52 +0530

    Create server middleware setup

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express=require("express");
-const mongoose=require("mongoose");
 const cors=require("cors");
 require("dotenv").config();
 
+const connectDB=require("./database/connection");
+
 const app=express();
 
-//
+app.use(cors());
+app.use(express.json());
+connectDB();
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message:"Libraray Management System API is running"
+    });
+});
+
+const PORT=process.env.PORT || 5001;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
