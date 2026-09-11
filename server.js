f9e7796e3b8c5288bf60846934074275a04fc72e
eshapatel2005 <esha.patel5605@gmail.com>
2026-09-11 16:21:15 +0530

    Connect Routes with Server

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ require("dotenv").config();
 
 const connectDB=require("./database/connection");
 
+const bookRoutes=require("./routes/books.routes");
+const userRoutes=require("./routes/users.routes");
+
 const app=express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/books".bookRoutes);
+app.use("/api/users".userRoutes);
+
 connectDB();
 
 app.get("/",(req,res)=>{
