7639389daae5f66627b479e912a86a1871115acb
eshapatel2005 <esha.patel5605@gmail.com>
2026-09-11 15:45:18 +0530

    Add Joi validation to user controller

---
+++ controller/user.js
@@ -0,0 +1,185 @@
+const { schema } = require("../models/book.model");
+const User=require("../models/user.model");
+const Joi=require("joi");
+
+//validation 
+const validateCreateUser=(data)=>{
+    const schema=Joi.object({
+        name:Joi.string().min(3).required().message({
+            "string.empty":"Please enter a valid name.",
+            "string.min":"Please enter a valid name.",
+            "any.required":"Please enter a valid name."
+        }),
+        email:Joi.string().email().required(),
+        phone:Joi.string().required()
+    });
+
+    return schema.Joi.validateCreateUser(data,{
+        convert:false
+    });
+};
+
+const validateGetUser=(data)=>{
+    const schema=Joi.object({
+        id:Joi.string().optional()
+    });
+    return schema.validate(data);
+};
+
+const validateUpdateUser=(data)=>{
+    const schem=Joi.object({
+        name:Joi.string().min(3),
+        email:Joi.string().email(),
+        phone:Joi.string()
+    });
+
+    return schema.validate(data);
+};
+
+//Create User 
+const createUser=async(req,res)=>{
+    try{
+        const{error}=validateCreateUser(req.body);
+
+        if(error){
+            return res.status(400).json({
+                success:false,
+                message:error.details[0].message
+            });
+        }
+        const user=await User.create(req.body);
+
+        res.status(201).json({
+            success:true,
+            message:"User created successfully",
+            data:user
+        });
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message
+        });
+    }
+};
+
+//Get All Users
+const getUsers=async(req,res)=>{
+    try{
+        const users=await User.find();
+
+        res.status(200).json({
+            success:true,
+            message:"Users Fetched Successfully",
+            data:users
+        });
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message
+        });
+    }
+};
+
+//Get Users By Id 
+const getUserById=async(req,res)=>{
+    try{
+        const {error}=validateGetUser(req.params);
+
+        if(error){
+            return res.status(400).json({
+                success:false,
+                message:error.details[0].message
+            });
+        }
+
+        const user=await User.findById(req.params.id);
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"User Not Found"
+            });
+        }
+
+        res.status(200).json({
+            success:true,
+            message:"User Fetched Successfully",
+            data:user
+        });
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message
+        });
+    }
+};
+
+//Update User 
+const updateUser=async(req,res)=>{
+    try{
+        const {error}=validateUpdateUser(req.body);
+
+        if(error){
+            return res.status(400).json({
+                success:false,
+                message:error.details[0].message
+            });
+        }
+
+        const user=await User.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        );
+
+        if (!user){
+            return res.status(404).json({
+                success:false,
+                message:"User Not Found"
+            });
+        }
+        res.status(200).json({
+            success:true,
+            message:"User Updated Successfully",
+            data:user
+        });
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message
+        });
+    }
+};
+
+// Delete User
+const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User Not Found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "User Deleted Successfully"
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
+
+
+module.exports = {
+    createUser,
+     getUsers,
+    getUserById,
+    updateUser,
+    deleteUser
+};
