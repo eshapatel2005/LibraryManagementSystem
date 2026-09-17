71b15f9f6885e523a2c85a4925a6f5f69fb9343e
eshapatel2005 <esha.patel5605@gmail.com>
2026-09-17 15:28:40 +0530

    Update User Routes Model

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -7,13 +7,24 @@ const {
     getUsers,
     getUserById,
     updateUser,
-    deleteUser
+    deleteUser,
+    loginUser
 } = require("../controller/user");
 
+const { auth } = require("../middleware/auth");
+
+// Create User
 router.post("/", createUser);
-router.get("/", getUsers);
-router.get("/:id", getUserById);
+// Login User
+router.post("/login", loginUser);
+// Get All Users
+router.get("/", auth, getUsers);
+// Get User By ID
+router.get("/:id", auth, getUserById);
+// Update User
 router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+// Delete User
+router.delete("/:id", auth, deleteUser);
+
 
 module.exports = router;
