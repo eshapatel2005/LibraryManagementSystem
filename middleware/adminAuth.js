5a4454bfb28b131014e185082a0293e7fd832af9
eshapatel2005 <esha.patel5605@gmail.com>
2026-09-22 16:57:05 +0530

    Formatted Document

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -2,40 +2,31 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 
 exports.adminAuth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization");
 
-    try {
+    const decode = jwt.verify(token, process.env.USER_AUTH_TOKEN);
 
-        const token = req.header("Authorization");
+    const findUser = await User.findById(decode.id);
 
-        const decode = jwt.verify(
-            token,
-            process.env.USER_AUTH_TOKEN
-        );
-
-        const findUser = await User.findById(decode.id);
-
-        if (!findUser) {
-            return res.status(401).json({
-                message: "User Not Found"
-            });
-        }
-
-        if (findUser.role !== "admin") {
-            return res.status(403).json({
-                message: "Admin Access Required"
-            });
-        }
-
-        req.user = findUser;
-
-        next();
-
-    } catch (error) {
-
-        res.status(401).json({
-            message: "Invalid Token"
-        });
-
+    if (!findUser) {
+      return res.status(401).json({
+        message: "User Not Found",
+      });
     }
 
+    if (findUser.role !== "admin") {
+      return res.status(403).json({
+        message: "Admin Access Required",
+      });
+    }
+
+    req.user = findUser;
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      message: "Invalid Token",
+    });
+  }
 };
