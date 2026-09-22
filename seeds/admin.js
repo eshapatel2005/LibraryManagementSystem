1391658623e3c3a9a31e6625314c49dd93ef3495
eshapatel2005 <esha.patel5605@gmail.com>
2026-09-22 14:15:08 +0530

    Admin.js formatted document

--- seeds/admin.js
+++ seeds/admin.js
@@ -1,44 +1,44 @@
-const mongoose=require("mongoose");
-const bcrypt=require("bcrypt");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 require("dotenv").config();
 
-const User=require("../models/user.model");
+const User = require("../models/user.model");
 
-const createAdmin=async()=>{
-    try{
-        //connect MongoDB
-        await mongoose.connect(process.env.MONGO_URI);
+const createAdmin = async () => {
+  try {
+    //connect MongoDB
+    await mongoose.connect(process.env.MONGO_URI);
 
-        //check admin Microsoft.QuickAction.WiFi
-        const admin=await User.findOne({
-            role:"admin"
-        });
+    //check admin
+    const admin = await User.findOne({
+      role: "admin",
+    });
 
-        if(admin){
-            console.log("Admin Already Exists");
-            process.exit();
-        }
-
-        //hash password 
-        const hashedPassword=await bcrypt.hash("Admin@1234",10);
-
-        //create Admin 
-        await User.create({
-            name:"Admin",
-            email:"admin@gmail.com",
-            phone:"9999999999",
-            password: hashedPassword,
-            role:"admin"
-        });
-
-        console.log("Admin Created Successfully");
-        process.exit();
-    }catch(error){
-        console.log("Admin Creation Failed");
-        console.log(error.message);
-
-        process.exit(1);
+    if (admin) {
+      console.log("Admin Already Exists");
+      process.exit();
     }
+
+    //hash password
+    const hashedPassword = await bcrypt.hash("Admin@1234", 10);
+
+    //create Admin
+    await User.create({
+      name: "Admin",
+      email: "admin@gmail.com",
+      phone: "9999999999",
+      password: hashedPassword,
+      role: "admin",
+    });
+
+    console.log("Admin Created Successfully");
+    process.exit();
+  } catch (error) {
+    console.log("Admin Creation Failed");
+    console.log(error.message);
+
+    process.exit(1);
+  }
 };
 
 createAdmin();
