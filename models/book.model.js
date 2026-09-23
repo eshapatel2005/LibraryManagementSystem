1750ac599a46dab63876ac05fc21aa451d2426dd
eshapatel2005 <esha.patel5605@gmail.com>
2026-09-23 14:39:24 +0530

    Add isIssued filed in schema

--- models/book.model.js
+++ models/book.model.js
@@ -1,41 +1,47 @@
 const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-            trim: true
-        },
-
-        author: {
-            type: String,
-            required: true,
-            trim: true
-        },
-
-        category: {
-            type: String,
-            required: true,
-            trim: true
-        },
-
-        isbn: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true
-        },
-
-        isIssued: {
-            type: Boolean,
-            default: false
-        }
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    
-    {
-        timestamps: true
-    }
+
+    author: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    isbn: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    isIssued: {
+      type: Boolean,
+      default: false,
+    },
+
+    issuedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+  },
+
+  {
+    timestamps: true,
+  },
 );
 
 module.exports = mongoose.model("Book", bookSchema);
